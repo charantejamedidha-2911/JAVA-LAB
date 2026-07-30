685f86e1d130242cf3265f1dccb059f9981b26e4
charantejamedidha-2911 <charantejamedidha@gmail.com>
2026-07-30 14:30:22 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,18 @@
+class Test
+{
+int a, b; 
+Test()
+{ 
+a=10; 
+b=20;
+System.out.println("Value of a: "+a); 
+System.out.println("Value of b: "+b);
+}
+}
+class TestDemo
+{
+public static void main(String args[])
+{
+Test t1=new Test();
+}
+}
